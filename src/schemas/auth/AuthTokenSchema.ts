743517e05f1ce5d40b9cc3ac3d8a58f1797ab062
root <root@M4FI4S-PC.>
2025-06-13 15:35:12 +0200

    fix(AuthTokenSchema): Corregir AuthToken

--- src/schemas/auth/AuthTokenSchema.ts
+++ src/schemas/auth/AuthTokenSchema.ts
@@ -6,10 +6,8 @@ import { z } from 'zod'
  * Expected format: "Bearer <base64 string>"
  * Example token generated with: `openssl rand -base64 64`
  */
-export const AuthTokenSchema = z.object({
-  token: z.string().regex(/^Bearer\s+[A-Za-z0-9+/=]{88}$/, {
-    message: 'Invalid Authorization header format',
-  }),
-})
+export const AuthTokenSchema = z
+  .string()
+  .regex(/^Bearer\s+[A-Za-z0-9+/=]{88}$/, { message: 'Invalid Authorization header format' })
 
 export type AuthToken = z.infer<typeof AuthTokenSchema>
